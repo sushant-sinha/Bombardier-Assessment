0469706ddd8a55fceaa11ac6364454cbb6f0b4fb
sushant-sinha <sinhasushant04@gmail.com>
2024-07-03 22:19:54 -0400

    added test for average

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 import pandas as pd
 import numpy as np
+from soln import fill_missing_values, calculate_average_glucose, diagnose_diabetes, normalize_data, calculate_mean_excluding_outliers
 
 class testDiabetesDiagnosisSteps(unittest.TestCase):
 
@@ -13,3 +14,12 @@ class testDiabetesDiagnosisSteps(unittest.TestCase):
             'cancerPresent': [1, 'False', 'True', 0, 'True', 'False'],
             'atrophy_present': [1, 0, 10, 1, 1, 0]
         })
+
+    # Calculate and test average glucose values
+    def test_calculate_average_glucose(self):
+        df = calculate_average_glucose(self.data.copy())
+        expected_averages = [110, 160, 210, 152.5, 189, 137.5]
+        self.assertTrue(np.allclose(df['average_glucose'], expected_averages))
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity=2, exit=False)
